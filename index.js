e24d77acd999055ee8a3c6436467c479fe6c5ae9
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-05-01 18:39:47 +0900

    Express Core: Middlewares

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const handleListening = () =>
   console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
+//클라이언트에게 실제로 반환되는
 
 const handleProfile = (req, res) => res.send("You are on my profile");
+
+//middleware
+const betweenHome = (req, res, next) => {
+  console.log("between");
+  //next 허용을 해줘야 handleHome으로 넘어갈 수 있음
+  next();
+};
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
