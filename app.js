33bba82f871d21e4cdc420d9acc813d0dedc329d
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-05-02 14:44:50 +0900

    Routing

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router"; //default 로 import하는 거랑 다름
+//export -> import
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass");
 //클라이언트에게 실제로 반환되는
@@ -17,8 +14,8 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
@@ -26,4 +23,7 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+/*누군가user라우터로 접속하면 유저라우터 전체를 사용하겠다*/
+
+export default app;
