af71b355965bbe582c7577b7d649087fc2bd1f00
hyunjinnnnnp <56399099+hyunjinnnnnp@users.noreply.github.com>
2020-05-10 14:03:47 +0900

    frontend part

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const ExtractCSS = require("extract-text-webpack-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+  entry: ["@babel/polyfill", ENTRY_FILE],
+  mode: MODE,
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(scss)$/,
+        use: ExtractCSS.extract([
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins() {
+                return [autoprefixer({ overrideBrowserslist: "cover 99.5%" })];
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ]),
+      },
+    ],
+  },
+  output: {
+    path: OUTPUT_DIR,
+    filename: "[name].js",
+  },
+  plugins: [new ExtractCSS("styles.css")],
+};
+
+module.exports = config;
+
+//모던자바스크립트 파일이아니기 때문에 옛날 방식으로 써야함
+//서버와는 전혀 관계없기때문에 바벨 노노함!
